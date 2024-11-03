ce9783f8006aec1921c095bd4f6f14fe3e6e3d43
pikapikapow <vladimirhwang84@gmail.com>
2024-11-03 10:21:27 -0500

    Completed a bunch more math exercises

--- exercises50-100.js
+++ exercises50-100.js
@@ -27,3 +27,59 @@
 // }
 
 // console.log(integervs20(0, 10, 50));
+
+// Function that sees if a given number is in the range of 40 to 10 thousand.
+
+// function isitin40to100(num) {
+//   if (num >= 40 && num <= 10000) {
+//     return "Yes, the number is above 40 and under 10000.";
+//   } else {
+//     return "Nope, the number is either below 40 or above 10000";
+//   }
+// }
+
+// console.log(isitin40to100(3200));
+
+// Function that runs division on two numbers
+
+// function divider(a, b) {
+//   let quotient = a / b;
+//   return quotient;
+// }
+
+// console.log(divider(9, 3));
+
+// function that runs quadratic formula on an equation.
+
+// function quadraticFormula(a, b, c) {
+//   const sqrt = Math.sqrt(b ** 2 - 4 * a * c);
+//   const quadraticAnswer1 = [b * -1 + sqrt] / (2 * a);
+//   const quadraticAnswer2 = [b * -1 - sqrt] / (2 * a);
+//   if (quadraticAnswer1 === quadraticAnswer2) {
+//     let solution = `The solution to an equation with an a of ${a}, a b of ${b}, and a c of ${c} that is in ax^2 + bx + c = 0 form is ${quadraticAnswer1}. `;
+//     return solution;
+//   } else {
+//     let solution = `The solutions to an equation with an a of ${a}, a b of ${b}, and a c of ${c} that is in ax^2 + bx + c = 0 form are ${quadraticAnswer1} and ${quadraticAnswer2}. `;
+//     return solution;
+//   }
+// }
+
+// console.log(quadraticFormula(1, 4, 4));
+
+// function that finds the discriminant of a quadratic equation and what type of answers it would have.
+
+// function discriminant(a, b, c) {
+//   const discriminant = b ** 2 - 4 * a * c;
+//   if (discriminant > 0) {
+//     let returnMessage = `The discriminant is ${discriminant} so there are two real answers.`;
+//     return returnMessage;
+//   } else if (discriminant === 0) {
+//     let returnMessage = `The discriminant is ${discriminant} so there is a repeated real answer.`;
+//     return returnMessage;
+//   } else {
+//     let returnMessage = `The discriminant is ${discriminant} so there are two complex answers.`;
+//     return returnMessage;
+//   }
+// }
+
+// console.log(discriminant(1, 5, 3));
